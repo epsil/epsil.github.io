c0db7e1aae00f0795a1d5b213a2f42bbf62ebacc
Vegard Øye <vegard_oye@hotmail.com>
2017-07-11 01:32:54 +0200

    Updated spotify

--- spotify/web.js
+++ spotify/web.js
@@ -3,9 +3,8 @@
 var $ = require('jquery')
 jQuery = $
 require('bootstrap')
-var defaults = require('../src/defaults')
-// var http = require('../src/http')
 var Parser = require('../src/parser')
+var SpotifyRequestHandler = require('../src/spotify')
 
 console.log = function (message) {
   if (typeof message === 'string') {
@@ -42,27 +41,32 @@ function resetButton () {
   console.log('')
 }
 
-function auth (clientId, uri) {
-  clientId = clientId || defaults.id
-  uri = uri || window.location.href
-  var url = 'https://accounts.spotify.com/authorize'
-  url += '/' +
-    '?client_id=' + encodeURIComponent(clientId) +
-    '&response_type=' + encodeURIComponent('token') +
-    '&redirect_uri=' + encodeURIComponent(uri)
-  return url
+function token () {
+  var hash = window.location.hash
+  hash = hash.replace(/^#/, '')
+  var urlParams = new URLSearchParams(hash)
+  if (!urlParams.has('access_token')) {
+    return ''
+  } else {
+    return urlParams.get('access_token')
+  }
+}
+
+function hasToken () {
+  return token() !== ''
 }
 
 function generate () {
   var textarea = $('textarea')
   var button = $('a.btn')
-  var generator = Parser(textarea.val(), token())
+  var parser = new Parser(token())
+  var generator = parser.parse(textarea.val())
   button.text('Creating Playlist \u2026')
   button.addClass('active')
   button.addClass('disabled')
   button.mouseleave()
   button.tooltip('disable')
-  generator.dispatch().then(function (result) {
+  generator.execute().then(function (result) {
     console.log('')
     button.removeClass('disabled')
     textarea.val(result)
@@ -79,21 +83,6 @@ function generate () {
   return false
 }
 
-function token () {
-  var hash = window.location.hash
-  hash = hash.replace(/^#/, '')
-  var urlParams = new URLSearchParams(hash)
-  if (!urlParams.has('access_token')) {
-    return ''
-  } else {
-    return urlParams.get('access_token')
-  }
-}
-
-function hasToken () {
-  return token() !== ''
-}
-
 function clickHandler () {
   if (hasToken()) {
     generate()
@@ -105,9 +94,10 @@ function clickHandler () {
 }
 
 $(function () {
+  var button = $('a.btn')
   $('.thumbnail a').click(insertPlaylist)
-  $('a.btn').click(clickHandler)
-  $('a.btn').tooltip()
+  button.click(clickHandler)
+  button.tooltip()
   $('textarea').focus()
   if (hasToken()) {
     if (localStorage.getItem('textarea')) {
@@ -115,6 +105,8 @@ $(function () {
       generate()
     }
   } else {
-    $('a.btn').attr('href', auth())
+    var spotify = new SpotifyRequestHandler()
+    var url = spotify.implicitGrantFlow(window.location.href)
+    button.attr('href', url)
   }
 })
